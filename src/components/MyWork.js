8897a292c67c2c1df0a505deb5cf6bead8a246fc
Joe Bates <joebates@jbimagine.com>
2018-12-15 00:08:26 -0800

    Start implementation of gallery

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -44,15 +44,16 @@ class MyWork extends Component {
             <ContentHeader title ='My Work' underline = {true}/>
         
         </MyWorkTitleContainer>
-        <div style = {{overflow:'hidden' }}>
-      
-            <div style = {{ display:'flex', justifyContent:'center', marginTop:'100px', overflow:'auto' }} >
+        <div style = {{overflow:'hidden', display:'flex',  alignItems:'center', height:'100%', position:'absolute', maxWidth:'100%' }}>
+        <div style = {{ background:'white', zIndex:'3', height:'100%', position:'absolute', width:'100px', backgroundColor:'#FFF' }} ></div>      
+            <div style = {{ display:'flex', justifyContent:'center', overflow:'hidden', gridColumn:'2',  position:'relative' }} >
                 {
                 this.state.myWorkItems.map( ( value, index ) => 
-                        <div style = {{ backgroundColor:'rgb(94,158,202)', height: '350px', width: '300px', display:'grid',margin:'0 32px', borderRadius:'10px', }}></div>
+                        <div  key = { index } style = {{ backgroundColor:'rgb(94,158,202)', height: '350px', width: '300px', display:'grid',margin:'0 32px', borderRadius:'10px', flexShrink:'0' }}></div>
                 )
                 }
             </div>
+            <div style = {{ background:'white', zIndex:'3', height:'100%', position:'absolute', width:'100px', backgroundColor:'#FFF', right:'0' }} ></div>      
         </div>
     </MyWorkContainer>
 
--- src/styled-components/MyWork_Styles.js
+++ src/styled-components/MyWork_Styles.js
@@ -4,12 +4,15 @@ import styled from 'styled-components';
 export const MyWorkContainer = styled.div `
     height:calc(100vh - 100px);
     background:#fff;
+    position:relative;
 `;
 
 export const MyWorkTitleContainer = styled.div `
     display:grid;
     justify-content:center;
     padding-top: 24px;
+    position:absolute;
+    width:100%;
 `
 
 export const MyWorkTitle = styled.div `
