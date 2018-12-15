f4602a4d6da1e06be9f4154bd0198d1434f6e8b5
Joe Bates <joebates@jbimagine.com>
2018-12-15 00:49:46 -0800

    Implement Fade in effect on edges of screen

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -45,15 +45,15 @@ class MyWork extends Component {
         
         </MyWorkTitleContainer>
         <div style = {{overflow:'hidden', display:'flex',  alignItems:'center', height:'100%', position:'absolute', maxWidth:'100%' }}>
-        <div style = {{ background:'white', zIndex:'3', height:'100%', position:'absolute', width:'100px', backgroundColor:'#FFF' }} ></div>      
-            <div style = {{ display:'flex', justifyContent:'center', overflow:'hidden', gridColumn:'2',  position:'relative' }} >
+        <div style = {{ zIndex:'3', height:'100%', position:'absolute', width:'100px', background:'linear-gradient(to right, rgba(255,255,255,1) 0%,rgba(255,255,255,0) 100%)' }} ></div>      
+            <div style = {{ display:'flex', justifyContent:'center', overflow:'hidden', gridColumn:'2',  position:'relative', width:'100vw' }} >
                 {
                 this.state.myWorkItems.map( ( value, index ) => 
                         <div  key = { index } style = {{ backgroundColor:'rgb(94,158,202)', height: '350px', width: '300px', display:'grid',margin:'0 32px', borderRadius:'10px', flexShrink:'0' }}></div>
                 )
                 }
             </div>
-            <div style = {{ background:'white', zIndex:'3', height:'100%', position:'absolute', width:'100px', backgroundColor:'#FFF', right:'0' }} ></div>      
+            <div style = {{ background:'linear-gradient(to left, rgba(255,255,255,1) 0%,rgba(255,255,255,0) 100%)', zIndex:'3', height:'100%', position:'absolute', width:'100px', right:'0' }} ></div>      
         </div>
     </MyWorkContainer>
 
